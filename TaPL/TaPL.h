f2b8d302bfb1aba6882dbb56aaaafdc17733d4e8
Rob Rix <rob.rix@github.com>
2014-08-25 22:00:05 -0400

    Initial Commit

---
+++ TaPL/TaPL.h
@@ -0,0 +1,19 @@
+//
+//  TaPL.h
+//  TaPL
+//
+//  Created by Rob Rix on 2014-08-25.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TaPL.
+FOUNDATION_EXPORT double TaPLVersionNumber;
+
+//! Project version string for TaPL.
+FOUNDATION_EXPORT const unsigned char TaPLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaPL/PublicHeader.h>
+
+
