9d43446c79f66184aff9bede18ac51b384ea63b1
Rob Rix <rob.rix@github.com>
2014-08-25 22:35:15 -0400

    parseTrue/parseFalse.

--- TaPL/3.swift
+++ TaPL/3.swift
@@ -27,3 +27,7 @@ func --> <T>(combinator: String -> (String, String)?, map: String -> T)(input: S
 
 	return nil
 }
+
+
+public let parseTrue = literal("true") --> { _ in Term.True }
+public let parseFalse = literal("false") --> { _ in Term.False }
