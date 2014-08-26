e73d55e6705e548a0de11d77c3a837c4a3d06709
Rob Rix <rob.rix@github.com>
2014-08-25 22:01:20 -0400

    Tidying.

--- TaPL/TaPL.h
+++ TaPL/TaPL.h
@@ -1,19 +1,7 @@
-//
-//  TaPL.h
-//  TaPL
-//
-//  Created by Rob Rix on 2014-08-25.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
 
 //! Project version number for TaPL.
-FOUNDATION_EXPORT double TaPLVersionNumber;
+extern double TaPLVersionNumber;
 
 //! Project version string for TaPL.
-FOUNDATION_EXPORT const unsigned char TaPLVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <TaPL/PublicHeader.h>
-
-
+extern const unsigned char TaPLVersionString[];
--- TaPLTests/TaPLTests.swift
+++ TaPLTests/TaPLTests.swift
@@ -1,36 +1,5 @@
-//
-//  TaPLTests.swift
-//  TaPLTests
-//
-//  Created by Rob Rix on 2014-08-25.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
 
-import Cocoa
 import XCTest
 
-class TaPLTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-}
+class TaPLTests: XCTestCase {}
