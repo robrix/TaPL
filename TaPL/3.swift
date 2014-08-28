504ce34c3f470a74a8ee14d33f0468ea75ca5f33
Rob Rix <rob.rix@github.com>
2014-08-27 20:19:20 -0400

    Move parsing to its own file.

--- TaPL/3.swift
+++ TaPL/3.swift
@@ -29,69 +29,6 @@ public func == (lhs: Term, rhs: Term) -> Bool {
 }
 
 
-/// A hack to provide a type-generic typealias for combinator functions.
-struct Combinator<T> {
-	typealias FunctionType = String -> (T, String)?
-}
-
-/// Constructs a literal parser for `string`.
-func literal(string: String)(input: String) -> (String, String)? {
-	if startsWith(input, string) {
-		return (string, input[advance(input.startIndex, countElements(string))..<string.endIndex])
-	}
-	return nil
-}
-
-
-postfix operator * {}
-
-/// Constructs a repetition parser.
-postfix func * <T>(combinator: String -> (T, String)?)(var input: String) -> String {
-	while let result = combinator(input) {
-		input = result.1
-	}
-	return input
-}
-
-infix operator --> {}
-
-/// Constructs a reduction parser, mapping parsed input into `T` via `map`.
-func --> <T>(combinator: String -> (String, String)?, map: String -> T)(input: String) -> (term: T, rest: String)? {
-	if let (parsed, rest) = combinator(input) { return (term: map(parsed), rest: rest) }
-
-	return nil
-}
-
-infix operator ++ {
-	associativity right
-	precedence 150
-}
-
-/// Constructs the concatenation of `lhs` and `rhs`.
-func ++ <T, U>(lhs: Combinator<T>.FunctionType, rhs: Combinator<U>.FunctionType)(input: String) -> ((T, U), String)? {
-	if let (lparsed, lrest) = lhs(input) {
-		if let (rparsed, rrest) = rhs(lrest) {
-			return ((lparsed, rparsed), rrest)
-		}
-	}
-	return nil
-}
-
-/// A binary choice.
-public enum Either<L, R> {
-	case Left(Box<L>)
-	case Right(Box<R>)
-}
-
-/// Constructs the alternation of `lhs` and `rhs`.
-func | <T, U>(lhs: Combinator<T>.FunctionType, rhs: Combinator<U>.FunctionType)(input: String) -> (Either<T, U>, String)? {
-	if let (parsed, rest) = lhs(input) { return (.Left(Box(parsed)), rest) }
-	if let (parsed, rest) = rhs(input) { return (.Right(Box(parsed)), rest) }
-	return nil
-}
-
-
-
 public let parseTrue = literal("true") --> { _ in Term.True }
 public let parseFalse = literal("false") --> { _ in Term.False }
 public let parseZero = literal("0") --> { _ in Term.Zero }
