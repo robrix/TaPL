224876be829343747d868516b3d09f99c0d333f5
Rob Rix <rob.rix@github.com>
2014-08-27 20:32:37 -0400

    Box Equality.

--- TaPL/Box.swift
+++ TaPL/Box.swift
@@ -1,9 +1,16 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+/// Ubiquitous, iniquitous.
 public final class Box<T> {
 	let value: T
 
 	init(_ value: T) {
 		self.value = value
 	}
+}
+
+
+/// Equality over Box<T : Equatable>
+public func == <T : Equatable> (lhs: Box<T>, rhs: Box<T>) -> Bool {
+	return lhs.value == rhs.value
 }
