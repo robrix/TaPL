3b189ba47c460119fcfb840519655de2df252808
Rob Rix <rob.rix@github.com>
2014-08-27 20:32:43 -0400

    Either Equality.

--- TaPL/Either.swift
+++ TaPL/Either.swift
@@ -5,3 +5,13 @@ public enum Either<L, R> {
 	case Left(Box<L>)
 	case Right(Box<R>)
 }
+
+
+/// Equality of Either<L : Equatable, R : Equatable>.
+public func == <L : Equatable, R : Equatable> (lhs: Either<L, R>, rhs: Either<L, R>) -> Bool {
+	switch (lhs, rhs) {
+	case let (.Left(x), .Left(y)) where x == y: return true
+	case let (.Right(x), .Right(y)) where x == y: return true
+	default: return false
+	}
+}
