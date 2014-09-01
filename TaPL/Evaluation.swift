ac4e1bb1b1adaa64fc8f00c5d35346fbaf2dfbe2
Rob Rix <rob.rix@github.com>
2014-09-01 18:44:52 -0400

    Evaluate IsZero terms.

--- TaPL/Evaluation.swift
+++ TaPL/Evaluation.swift
@@ -1,5 +1,11 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 public func eval(term: Term) -> Term {
-	return term
+	switch term {
+	case let .IsZero(x):
+		return eval(x.value) == Term.Zero ? Term.True : Term.False
+
+	default:
+		return term
+	}
 }
