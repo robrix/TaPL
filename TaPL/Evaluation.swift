e0445d1dad20004efbcc633dc7c3b7cd80167595
Rob Rix <rob.rix@github.com>
2014-09-01 22:39:49 -0400

    Terms are booleans. Sorta.

--- TaPL/Evaluation.swift
+++ TaPL/Evaluation.swift
@@ -1,5 +1,14 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+extension Term : BooleanType {
+	public var boolValue: Bool {
+		switch self {
+		case .True: return true
+		default: return false
+		}
+	}
+}
+
 public func eval(term: Term) -> Term {
 	switch term {
 	case let .IsZero(x):
