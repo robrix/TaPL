cf0d3c2a1a94bba9a0c056c8b86fb42a6e6014e9
Rob Rix <rob.rix@github.com>
2014-09-01 23:16:59 -0400

    Evaluate.

--- repl-3/main.swift
+++ repl-3/main.swift
@@ -20,7 +20,7 @@ while let line = prompt() {
 	if line == ":exit" { break }
 
 	if let (term, rest) = parseTerm(input: line) {
-		println(term)
+		println(eval(term))
 	} else {
 		println("onoes")
 	}
