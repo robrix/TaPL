1246317daf9fabc17d687496b5e4e0f84a113800
Rob Rix <rob.rix@github.com>
2014-09-01 22:39:55 -0400

    Evaluation of If.

--- TaPL/Evaluation.swift
+++ TaPL/Evaluation.swift
@@ -14,6 +14,9 @@ public func eval(term: Term) -> Term {
 	case let .IsZero(x):
 		return eval(x.value) == Term.Zero ? Term.True : Term.False
 
+	case let .If(x, y, z):
+		return eval(x.value) ? eval(y.value) : eval(z.value)
+
 	default:
 		return term
 	}
