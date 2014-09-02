15535ba9a39e34ca0b99b6e4e253723e73c001d7
Rob Rix <rob.rix@github.com>
2014-09-01 22:17:33 -0400

    readline() and prompt() functions.

--- repl-3/main.swift
+++ repl-3/main.swift
@@ -2,3 +2,17 @@
 
 import Foundation
 
+func readline(handle: UnsafeMutablePointer<FILE>) -> String? {
+	let getNonNewline: () -> CChar? = {
+		let c = CChar(getc(handle))
+		return String.fromCString([c, 0]) == "\n" ? nil : c
+	}
+
+	return String.fromCString(Array(SequenceOf(GeneratorOf(getNonNewline))))
+}
+
+func prompt() -> String? {
+	print("> ")
+	return readline(stdin)
+}
+
