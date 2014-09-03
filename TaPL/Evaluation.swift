1492594f84c02dc778d5945f5548ea719058439c
Rob Rix <rob.rix@github.com>
2014-09-03 18:37:26 -0400

    eval returns Optional.

--- TaPL/Evaluation.swift
+++ TaPL/Evaluation.swift
@@ -18,22 +18,27 @@ extension Optional {
 	}
 }
 
-public func eval(term: Term) -> Term {
+public func eval(term: Term) -> Term? {
 	switch term.destructure() {
-	case let .Successor(x): return .Successor(Box(eval(x))) // E-Succ
+	// values
+	case .True: return .True
+	case .False: return .False
+	case .Zero: return .Zero
+
+	case let .Successor(x): return eval(x).map{ .Successor(Box($0)) } // E-Succ
 
 	case .Predecessor(.Zero): return .Zero // E-PredZero
 	case let .Predecessor(.Successor(x)) where x.value.numeric: return x.value // E-PredSucc
-	case let .Predecessor(x): return eval(.Predecessor(Box(eval(x)))) // E-Pred
+	case let .Predecessor(x): return eval(x).bind { eval(.Predecessor(Box($0))) } // E-Pred
 
 	case .IsZero(.Zero): return .True // E-IsZeroZero
 	case let .IsZero(.Successor(x)) where x.value.numeric: return .False // E-IsZeroSucc
-	case let .IsZero(x): return eval(.IsZero(Box(eval(x)))) // E-IsZero
+	case let .IsZero(x): return eval(x).bind { eval(.IsZero(Box($0))) } // E-IsZero
 
 	case let .If(.True, x, _): return eval(x) // E-IfTrue
 	case let .If(.False, _, x): return eval(x) // E-IfFalse
-	case let .If(x, y, z): return eval(.If(Box(eval(x)), Box(y), Box(z))) // E-If
+	case let .If(x, y, z): return eval(x).bind { eval(.If(Box($0), Box(y), Box(z))) } // E-If
 
-	default: return term // .True, .False, and .Zero are already values
+	default: return nil
 	}
 }
