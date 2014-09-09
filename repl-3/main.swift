abf3eb000849e7947b6b209c9158dcf67073897a
Rob Rix <rob.rix@github.com>
2014-09-09 03:19:04 -0400

    Extract REPL stuff into its own file.

--- repl-3/main.swift
+++ repl-3/main.swift
@@ -2,30 +2,10 @@
 
 import Darwin
 
-func readline(handle: UnsafeMutablePointer<FILE>) -> String? {
-	let getNonNewline: () -> CChar? = {
-		let c = CChar(getc(handle))
-		return String.fromCString([c, 0]) == "\n" ? nil : c
-	}
-
-	return String.fromCString(Array(SequenceOf(GeneratorOf(getNonNewline))))
-}
-
-func prompt() -> String? {
-	print("> ")
-	return readline(stdin)
-}
-
-while let line = prompt() {
-	if line == ":exit" { break }
-
-	if let (term, rest) = parseTerm(input: line) {
-		if let value = eval(term) {
-			println(value)
-		} else {
-			println("stuck")
-		}
+repl(parseTerm) {
+	if let value = eval($0) {
+		println(value)
 	} else {
-		println("onoes")
+		println("stuck")
 	}
 }
