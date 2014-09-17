1d398261355d723169357d6121a38eb248083c9e
Rob Rix <rob.rix@github.com>
2014-09-17 18:33:42 -0400

    Parse left-recursively, and thus infinite loop.

--- λ/λ.swift
+++ λ/λ.swift
@@ -1,27 +1,33 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-public struct Variable {
-	let name: String
-}
-
-public enum Term : Equatable {
-	case Variable(λ.Variable)
-	case Abstraction(λ.Variable, Box<Term>)
+enum Term : Equatable, Printable {
+	case Variable(String)
+	case Abstraction(String, Box<Term>)
 	case Application(Box<Term>, Box<Term>)
+
+	var description: String {
+		switch self {
+		case let .Variable(x): return x
+		case let .Abstraction(x, y): return "\\ \(x) . \(y.value)"
+		case let .Application(x, y): return "\(x.value) \(y.value)"
+		}
+	}
 }
 
-public func == (lhs: Term, rhs: Term) -> Bool {
+func == (lhs: Term, rhs: Term) -> Bool {
 	return false
 }
 
+let parseOptionalWhitespace = ignore(literal(" ")*)
+let parseRequiredWhitespace = ignore(literal(" ") ++ literal(" ")*)
+let parseVariableTerm = parseVariable --> { Term.Variable($0) }
+let parseTerm: Combinator<Term>.FunctionType = fix { parseAbstraction | parseApplication | parseVariableTerm }
 
+let parseVariable = characterSet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-// fixme: file a radar about the deadlock here
-public let parseWhitespace = ignore(literal(" ")*)
-public let parseVariableTerm = parseVariable --> { Term.Variable($0) }
-public let parseTerm: Combinator<Term>.FunctionType = parseVariableTerm | parseAbstraction | parseApplication
+let parseLambda = ignore(literal("\\") ++ parseOptionalWhitespace)
+let parseParameter = parseVariable ++ parseOptionalWhitespace
+let parseBody = ignore(literal(".")) ++ parseOptionalWhitespace ++ parseTerm ++ parseOptionalWhitespace
+let parseAbstraction = (parseLambda ++ parseParameter ++ parseBody) --> { Term.Abstraction($0, Box($1)) }
 
-public let parseVariable = parseSet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") --> { Variable(name: $0) }
-public let parseAbstraction = (ignore(literal("λ")) ++ parseVariable ++ ignore(literal(".")) ++ parseTerm) --> { Term.Abstraction($0, Box($1)) }
-
-public let parseApplication = (parseTerm ++ parseWhitespace ++ parseTerm) --> { Term.Application(Box($0), Box($1)) }
+let parseApplication = (parseTerm ++ parseRequiredWhitespace ++ parseTerm) --> { Term.Application(Box($0), Box($1)) }
