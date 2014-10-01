9a917d6e72d538ea3d0ab075ba3bfad6ac592582
Rob Rix <rob.rix@github.com>
2014-10-01 18:28:43 -0400

    Pretty-print nameless terms.

--- λ/DeBruijn.swift
+++ λ/DeBruijn.swift
@@ -4,4 +4,12 @@ enum NamelessTerm {
 	case Index(Int)
 	case Abstraction(Box<NamelessTerm>)
 	case Application(Box<NamelessTerm>, Box<NamelessTerm>)
+
+	var description: String {
+		switch self {
+		case let .Index(i): return "\(i)"
+		case let .Abstraction(t): return "\\. \(t.value.description)"
+		case let .Application(t, u): return "\(t.value) \(u.value)"
+		}
+	}
 }
