9728c10a8f185fef44371a8de1d5e601653fd061
Rob Rix <rob.rix@github.com>
2014-11-17 18:23:26 -0500

    Include things in the simple target.

--- simple/main.swift
+++ simple/main.swift
@@ -1,12 +1,4 @@
-//
-//  main.swift
-//  simple
-//
-//  Created by Rob Rix on 2014-11-17.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
 
-import Foundation
-
-println("Hello, World!")
+// Simply typed λ calculus, ch 9.
 
