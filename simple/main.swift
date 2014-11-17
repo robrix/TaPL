866e4775a87f18831f4af35062c72554c48144e5
Rob Rix <rob.rix@github.com>
2014-11-17 18:21:05 -0500

    Add a `simple` target.

---
+++ simple/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  simple
+//
+//  Created by Rob Rix on 2014-11-17.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
