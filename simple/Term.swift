5929ee5528d38fec3cb69b69c38e3ff8d2670a26
Rob Rix <rob.rix@github.com>
2014-11-26 18:15:24 -0500

    Term construction.

--- simple/Term.swift
+++ simple/Term.swift
@@ -1,5 +1,21 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+infix operator • {}
+func • (lhs: Term<()>, rhs: Term<()>) -> Term<()> {
+	return .Application(Box(()), Box(lhs), Box(rhs))
+}
+
+prefix operator % {}
+
+prefix func %(x: Int) -> Term<()> {
+	return Term.Index(Box(()), x)
+}
+
+func lambda(x: Term<()>) -> Term<()> {
+	return Term.Abstraction(Box(()), Box(x))
+}
+
+
 enum Term<Info> {
 	case Index(Box<Info>, Int)
 	case Abstraction(Box<Info>, Box<Term>)
