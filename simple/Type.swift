3feaf521f2368ebeb18991ba96d865116b67af14
Rob Rix <rob.rix@github.com>
2014-12-03 20:35:22 -0500

    typeof takes a context.

--- simple/Type.swift
+++ simple/Type.swift
@@ -9,6 +9,6 @@ func --> (lhs: Type, rhs: Type) -> Type {
 	return .Function(Box(lhs), Box(rhs))
 }
 
-func typeof<Info>(term: Term<Info>) -> Type {
+func typeof<Info>(term: Term<Info>, context: [(Int, Type)] = []) -> Type {
 	return Type.Bool
 }
