b95b498281f1502f1669e216ec610660ddd4c87d
Rob Rix <rob.rix@github.com>
2014-12-03 20:42:21 -0500

    Type is Equatable.

--- simple/Type.swift
+++ simple/Type.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-enum Type {
+enum Type: Equatable {
 	case Bool
 	case Function(Box<Type>, Box<Type>)
 }
@@ -11,4 +11,18 @@ func --> (lhs: Type, rhs: Type) -> Type {
 
 func typeof<Info>(term: Term<Info>, context: [(Int, Type)] = []) -> Type {
 	return Type.Bool
+}
+
+
+// MARK: Equatable
+
+func == (left: Type, right: Type) -> Bool {
+	switch (left, right) {
+	case (.Bool, .Bool):
+		return true
+	case let (.Function(x1, y1), .Function(x2, y2)) where x1 == x2 && y1 == y2:
+		return true
+	default:
+		return false
+	}
 }
