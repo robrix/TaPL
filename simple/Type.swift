ecf380ad7f79a7845ccacd6d209f0b2939bebec3
Rob Rix <rob.rix@github.com>
2014-12-03 21:38:06 -0500

    Use Either for error reporting.

--- simple/Type.swift
+++ simple/Type.swift
@@ -18,47 +18,59 @@ func --> (lhs: Type, rhs: Type) -> Type {
 	return .Function(Box(lhs), Box(rhs))
 }
 
-func typeof(term: Term<()>, context: [(Int, Type)] = []) -> Type {
-	let recur: Box<Term<()>> -> Type = { term in
+extension Either {
+	func map<V>(f: U -> V) -> Either<T, V> {
+		return either(Either<T, V>.left, f >>> Either<T, V>.right)
+	}
+}
+
+infix operator ** {}
+func ** <T, U>(left: Either<T, U>, right: Either<T, U>) -> Either<T, (U, U)> {
+	return left.either(Either<T, (U, U)>.left, { x in
+		right.map { y in
+			(x, y)
+		}
+	})
+}
+
+func typeof(term: Term<()>, context: [(Int, Type)] = []) -> Either<String, Type> {
+	let recur: Box<Term<()>> -> Either<String, Type> = { term in
 		typeof(term.value, context: context)
 	}
 
 	switch term {
 	case .True, .False:
-		return .Bool
+		return .right(.Bool)
 
 	case let .If(_, condition, then, otherwise):
-		let conditionType = recur(condition)
-		if conditionType == .Bool {
-			let (thenType, elseType) = (recur(then), recur(otherwise))
-			if thenType == elseType {
-				return thenType
-			} else {
-				println("error: branches had inequal types: \(thenType) != \(elseType)")
-			}
-		} else {
-			println("error: condition had type \(conditionType) instead of Bool")
-		}
-		return .Bool
+		return recur(condition).either(Either.left, { conditionType in
+			conditionType == .Bool ?
+				((recur(then) ** recur(otherwise))).either(Either.left, { thenType, elseType in
+					thenType == elseType ?
+						.right(thenType)
+					:	.left("error: branches had inequal types: \(thenType) != \(elseType)")
+				})
+			:	.left("error: condition had type \(conditionType) instead of Bool")
+		})
 
 	case let .Index(_, i):
-		return context[i].1
+		return .right(context[i].1)
 
 	case let .Abstraction(_, t, v):
-		return t --> typeof(v.value, context: context + [(context.count, t)])
+		return typeof(v.value, context: context + [(context.count, t)]).map {
+			t --> $0
+		}
 
 	case let .Application(_, a, b):
-		if let (parameterType, returnType) = recur(a).functionType {
-			let argumentType = recur(b)
-			if parameterType == argumentType {
-				return returnType
-			} else {
-				print("error: argument had type \(argumentType) instead of expected type \(parameterType)")
-			}
-		} else {
-			println("error: \(a) is not of function type")
-		}
-		return .Bool
+		return recur(a).either(Either.left, {
+			$0.functionType.map { parameterType, returnType in
+				recur(b).either(Either.left, { argumentType in
+					argumentType == parameterType ?
+						.right(returnType)
+					:	.left("error: argument had type \(argumentType) instead of expected type \(parameterType)")
+				})
+			} ?? .left("error: \(a) is not of function type")
+		})
 	}
 }
 
@@ -80,3 +92,5 @@ func == (left: Type, right: Type) -> Bool {
 // MARK: Import
 
 import Box
+import Either
+import Prelude
