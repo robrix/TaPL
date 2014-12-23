3685265f3a376523db1ed85dd629143cac974324
Rob Rix <rob.rix@github.com>
2014-12-23 16:32:47 -0500

    Not quite.

--- simple/main.swift
+++ simple/main.swift
@@ -1,7 +1,5 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-// Simply typed λ calculus, ch 9.
-
 let index = %0
 let application = index • %true
 let abstraction = lambda(.Bool --> .Bool, application)
