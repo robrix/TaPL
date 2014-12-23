84dc762b5b6cc349679a09fe9e95f0878c6400ed
Rob Rix <rob.rix@github.com>
2014-12-23 17:34:54 -0500

    Simple parser for the lambda calculus.

---
+++ simple/Parser.swift
@@ -0,0 +1,21 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+let digit: Parser<Int>.Function = %("0"..."9") --> { strtol($0, nil, 10) }
+let boolean: Parser<Type>.Function = %"Bool" --> const(Type.Bool)
+let function: Parser<Type>.Function = ignore("(") ++ type ++ ignore("->") ++ type ++ ignore(")") --> { Type.Function(Box($0), Box($1)) }
+let type: Parser<Type>.Function = boolean | function
+
+let term: Parser<Term<()>>.Function = fix { term in
+	let variable = digit --> { Term.Index(Box(()), $0) }
+	let abstraction = ignore("λ") ++ type ++ ignore(".") ++ term --> { Term.Abstraction(Box(()), $0, Box($1)) }
+	let application = ignore("(") ++ term ++ ignore(" ") ++ term ++ ignore(")") --> { Term.Application(Box(()), Box($0), Box($1)) }
+	return variable | abstraction | application
+}
+
+
+// MARK: - Imports
+
+import Box
+import Darwin.C.stdlib
+import Madness
+import Prelude
