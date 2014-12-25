cf3e23a9501c3245586e4e30f75a3a877a83e633
Rob Rix <rob.rix@github.com>
2014-12-24 19:15:36 -0500

    Whitespace.

--- TaPL/Parser.swift
+++ TaPL/Parser.swift
@@ -1,6 +1,8 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 let digit: Parser<Int>.Function = %("0"..."9") --> { strtol($0, nil, 10) }
+let whitespace: Parser<()>.Function = ignore(%" " | %"\r" | %"\t" | %"\n")
+
 let type: Parser<Type>.Function = fix { type in
 	let boolean: Parser<Type>.Function = %"Bool" --> const(Type.Bool)
 	let function: Parser<Type>.Function = ignore("(") ++ type ++ ignore("->") ++ type ++ ignore(")") --> { Type.Function(Box($0), Box($1)) }
@@ -11,7 +13,7 @@ public let parseTerm: Parser<Term<()>>.Function = fix { term in
 	let literal = (%"true" --> const(Term.True(Box(())))) | (%"false" --> const(Term.False(Box(()))))
 	let variable = digit --> { Term.Index(Box(()), $0) }
 	let abstraction = ignore("λ") ++ type ++ ignore(".") ++ term --> { Term.Abstraction(Box(()), $0, Box($1)) }
-	let application = ignore("(") ++ term ++ ignore(" ") ++ term ++ ignore(")") --> { Term.Application(Box(()), Box($0), Box($1)) }
+	let application = ignore("(") ++ term ++ whitespace ++ term ++ ignore(")") --> { Term.Application(Box(()), Box($0), Box($1)) }
 	return literal | variable | abstraction | application
 }
 
